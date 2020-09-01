4e5d09aa4ed6315491abce489d1b7d7ea4506f42
kaiyuanbisheng <maxrootmin@163.com>
2020-09-01 16:43:25 +0800

    第一次提交

---
+++ src/components/index/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="list">1
+     <van-tabs v-model="active" swipeable>
+        <van-tab v-for="index in 4" :title="'选项 ' + index" :key="index">
+            内容 {{ index }}
+        </van-tab>
+      </van-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+};
+</script>
+<style lang='scss' scoped></style>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,28 @@ import store from './store'
 
 import "amfe-flexible";
 
-import { Button, Tabbar, TabbarItem } from 'vant';
-Vue.use(Button);
+import {
+    Button,
+    Tabbar,
+    TabbarItem,
+    Search,
+    Swipe,
+    SwipeItem,
+    Grid,
+    GridItem,
+    Tab,
+    Tabs
+} from 'vant';
+Vue.use(Tab)
+Vue.use(Tabs) //tab
+Vue.use(Grid)
+Vue.use(GridItem) //宫格
+Vue.use(SwipeItem) //轮播
+Vue.use(Swipe) //轮播
+Vue.use(Button); //按钮
 Vue.use(Tabbar); //Tabbar 标签栏
 Vue.use(TabbarItem); //Tabbar 标签栏
+Vue.use(Search) //搜索
 
 Vue.config.productionTip = false
 
