fb8acb2285b8397d1e7eb2461a28b020c477acd7
luren66666666 <2965890504@qq.com>
2020-09-02 22:04:18 +0800

    1

--- src/components/theticketimmediately/TheTicketImmediately.vue
+++ src/components/theticketimmediately/TheTicketImmediately.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="Buy-tickets-immediately">
+    <div class="Buy-tickets-immediately" >
         <!-- 顶部导航栏 -->
-        <van-nav-bar title="立即购票"  left-arrow :border="false">
+        <van-nav-bar title="立即购票"  left-arrow fixed :placeholder="true"  >
             <template #right>
                 <van-icon name="warning-o" size="18" />
             </template>
         </van-nav-bar>
+        <p class="ptime">选择场次</p>
+        <div class="divtime"></div>
+        <div class="divtime"></div>
     </div>
 </template>
 <script>
@@ -14,9 +17,12 @@ export default {
 }
 </script>
 <style lang="scss">
+// @import '@/assets/style/reset.scss';
     .Buy-tickets-immediately{
+        // background-color: white;
         .van-nav-bar{
             height: 36px;
+            // border-bottom: 1px solid #A0A0A0;
             .van-nav-bar__left{
                 .van-icon{
                     font-size: 23px;
@@ -35,6 +41,21 @@ export default {
                     color: black;
                 }
             }
+        }
+        .ptime{
+            font-size: 14px;
+            font-family: Medium;
+            color: #040000;
+            margin-top: 22px;
+            margin-left: 15px;
+            margin-bottom: 13px;
+        }
+        .divtime{
+            width: 346px;
+            height: 38px;
+            background-color: #FFF1F4;
+            margin-left: 15px;
+            margin-top: 10px;
         }
     }
 </style>
