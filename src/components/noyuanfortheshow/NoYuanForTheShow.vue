6073ea9ea0aaa061b9afbead4bee99b401fec465
luren66666666 <2965890504@qq.com>
2020-09-07 16:12:55 +0800

    222

--- src/components/noyuanfortheshow/NoYuanForTheShow.vue
+++ src/components/noyuanfortheshow/NoYuanForTheShow.vue
@@ -1,10 +1,68 @@
 <template>
-    <div>
-        03和未标题-1（霸王票0元任性看演出）
+    <div class="no-yuan-for-the-show">
+        <!-- 顶部导航栏 -->
+        <van-nav-bar title="霸王票0元任性看演出"  left-arrow :border="false" @click-left="onClickLeft">
+            <template #right>
+                <van-icon name="cluster-o" size="18" />
+            </template>
+        </van-nav-bar>
+
+
+        <div class="no-yuan-for-the-show-img">
+            <img src="" alt="">
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    methods: {
+        onClickLeft(){
+            this.$router.go(-1);
+        }
+    },
 }
 </script>
+<style lang="scss">
+    .no-yuan-for-the-show{
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .van-nav-bar{
+            width: 100%;
+            background-color: white;
+            height: 31px;
+            .van-icon{
+                color: black;
+            }
+            .van-nav-bar__title{
+                font-size: 16px;
+                font-family: Regular;
+                margin-left: 110px;
+                color: #494949;
+                font-weight: 600;
+            }
+            .van-nav-bar__left{
+                margin-left: 12px;
+                .van-icon{
+                    font-size: 20px;
+                }   
+            }
+            .van-nav-bar__right{
+                margin-right: 20px;
+                .van-icon{
+                    font-size: 20px;
+                }
+            }
+        }
+        .no-yuan-for-the-show-img{
+            width: 100%;
+            height: 0;
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+</style>
--- src/components/thescene/TheScene.vue
+++ src/components/thescene/TheScene.vue
@@ -1,10 +1,68 @@
 <template>
-    <div>
-        01(十分现场)
+    <div class="the-scene">
+        <!-- 顶部导航栏 -->
+        <van-nav-bar title="十分现场"  left-arrow :border="false" @click-left="onClickLeft">
+            <template #right>
+                <van-icon name="cluster-o" size="18" />
+            </template>
+        </van-nav-bar>
+
+
+        <div class="the-scene-img">
+            <img src="" alt="">
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    methods: {
+        onClickLeft(){
+            this.$router.go(-1);
+        }
+    },
 }
 </script>
+<style lang="scss">
+    .the-scene{
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .van-nav-bar{
+            width: 100%;
+            background-color: white;
+            height: 33px;
+            .van-icon{
+                color: black;
+            }
+            .van-nav-bar__title{
+                font-size: 17px;
+                font-family: Regular;
+                margin-left: 156px;
+                color: #4A4A4A;
+                font-weight: 600;
+            }
+            .van-nav-bar__left{
+                margin-left: 17px;
+                .van-icon{
+                    font-size: 20px;
+                }   
+            }
+            .van-nav-bar__right{
+                margin-right: 13px;
+                .van-icon{
+                    font-size: 20px;
+                }
+            }
+        }
+        // height: 0;
+        .the-scene-img{
+            width: 100%;
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+</style>
