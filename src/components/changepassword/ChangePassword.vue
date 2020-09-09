380a7ebd2eb2e3c106bbb8ae1217b077df443b32
luren66666666 <2965890504@qq.com>
2020-09-09 20:23:00 +0800

    11

--- src/components/changepassword/ChangePassword.vue
+++ src/components/changepassword/ChangePassword.vue
@@ -1,10 +1,103 @@
 <template>
-    <div>
-        修改密码
+    <div class="change-passwords">
+        <!-- 顶部导航栏 -->
+        <van-nav-bar title="重置密码"  left-arrow :border="false" @click-left="onClickLeft">
+        </van-nav-bar>
+
+
+        <div class="change-passwords-color">
+            <div class="text-from">
+                <span class="tel">手机号</span><span></span>
+                <div class="xian"></div>
+                <span class="code">验证码</span><span></span>
+            </div>
+            <div class="next">
+                <span>下一步</span>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    methods: {
+        onClickLeft(){
+            this.$router.go(-1);
+        }
+    },
 }
 </script>
+<style lang="scss">
+    .change-passwords{
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .van-nav-bar{
+            width: 100%;
+            height: 33px;
+            background-color: #FBFBFB;
+            .van-icon{
+                font-size: 20px;
+                color: black;
+            }
+            .van-nav-bar__title{
+                color: #515151;
+                font-size: 14px;
+                font-family: Medium;
+                margin-left: 156px;
+            }
+            .van-nav-bar__left{
+                margin-left: 18px;
+            }
+        }
+        .change-passwords-color{
+            position: fixed;
+            left: 0;
+            top: 33px;
+            right: 0;
+            bottom: 0;
+            background-color: #F1F1F1;
+            .text-from{
+                width: 100%;
+                height: 79px;
+                background-color: #FFFFFF;
+                position: absolute;
+                left: 0px;
+                top: 18px;
+                .tel{
+                    color: #000000;
+                    font-size: 12px;
+                    font-family: Regular;
+                    position: absolute;
+                    left: 15px;
+                    top: 14px;
+                }
+                .code{
+                    color: #000000;
+                    font-size: 12px;
+                    font-family: Regular;
+                    position: absolute;
+                    left: 14px;
+                    top: 53px;
+                }
+            }
+            .next{
+                width: 348px;
+                height: 44px;
+                background-color: #ff2661;
+                position: absolute;
+                left: 12px;
+                top: 116px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                span{
+                    color: #FFFFFF;
+                    font-size: 16px;
+                    font-family: Regular;
+                }
+            }
+        }
+    }
+</style>
--- src/components/resetpasswords/ResetPasswords.vue
+++ src/components/resetpasswords/ResetPasswords.vue
@@ -1,13 +1,42 @@
 <template>
-    <div>
-        重置密码
+    <div class="reset-passwords">
+        <!-- 顶部导航栏 -->
+        <van-nav-bar title="艳阳高照 组团出门浪"  left-arrow :border="false" @click-left="onClickLeft">
+            <template #right>
+                <van-icon name="cluster-o" size="18" />
+            </template>
+        </van-nav-bar>
     </div>
 </template>
 <script>
 export default {
-    
+    methods: {
+        onClickLeft(){
+            this.$router.go(-1);
+        }
+    },
 }
 </script>
 <style lang="scss">
-    
+    .reset-passwords{
+        .van-nav-bar{
+            width: 100%;
+            background-color: #efa383;
+            height: 36px;
+            .van-icon{
+                color: black;
+            }
+            .van-nav-bar__title{
+                font-size: 16px;
+                font-family: Bold;
+                margin-left: 115px;
+            }
+            .van-nav-bar__left{
+                margin-left: 18px;
+            }
+            .van-nav-bar__right{
+                margin-right: 22px;
+            }
+        }
+    }
 </style>
